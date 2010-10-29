950873310dec3491acae3750a922754761519550
Chris Chambers <magma.chambers@gmail.com>
2010-10-29 15:19:53 +0100

    Refactoring: validators

--- calendartools/validators.py
+++ calendartools/validators.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from django.core.exceptions import ValidationError
 from calendartools.signals import collect_occurrence_validators
 
+
 class BaseValidator(object):
     priority = 10
 
@@ -13,27 +14,21 @@ class BaseValidator(object):
         """Raises ValidationError if validation is unsuccessful."""
         raise NotImplementedError
 
-def finish_gt_start(sender, **kwargs):
-    if sender.start >= sender.finish:
-        raise ValidationError(
-            'Finish date/time must be greater than start date/time.'
-        )
-
-def create_occurrences_in_future_only(sender, **kwargs):
-    if not sender.id and sender.start < datetime.now():
-        raise ValidationError(
-            'Event senders cannot be created in the past.'
-        )
-
 
 class FinishGTStartValidator(BaseValidator):
     def validate(self):
-        return finish_gt_start(self.sender, **self.kwargs)
+        if self.sender.start >= self.sender.finish:
+            raise ValidationError(
+                'Finish date/time must be greater than start date/time.'
+            )
 
 
 class FutureOccurrencesOnlyValidator(BaseValidator):
     def validate(self):
-        return create_occurrences_in_future_only(self.sender, **self.kwargs)
+        if not self.sender.id and self.sender.start < datetime.now():
+            raise ValidationError(
+                'Event occurrences cannot be created in the past.'
+            )
 
 def activate_default_validators():
     collect_occurrence_validators.connect(
