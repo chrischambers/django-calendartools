bea401451ea6f7ed38cbcc5c018d573e7ae33fe0
Chris Chambers <magma.chambers@gmail.com>
2010-11-07 21:27:44 +0000

    Columns - improvement

--- calendartools/templatetags/math.py
+++ calendartools/templatetags/math.py
@@ -2,6 +2,9 @@ from django import template
 
 register = template.Library()
 
+# Unused:
+# ------------------------------------------------------------
+
 @register.filter(name='divide_by')
 def divide_by(value, arg):
     try:
@@ -30,3 +33,32 @@ make_range.is_safe = True
 @register.filter(name='get_index')
 def get_index(container, index):
     return container[index]
+
+# ------------------------------------------------------------
+
+@register.filter
+def columns(lst, cols):
+    """
+    Break a list into ``n`` lists, typically for use in columns.
+    Sources:
+        http://brandonkonkle.com/blog/2010/may/26/snippet-django-columns-filter/
+        http://herself.movielady.net/2008/07/16/split-list-to-columns-django-template-tag/
+
+    >>> lst = range(10)
+    >>> for list in columns(lst, 3):
+    ...     list
+    [0, 1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+    """
+    try:
+        cols = int(cols)
+        lst = list(lst)
+    except (ValueError, TypeError):
+        raise StopIteration()
+
+    start = 0
+    for i in xrange(cols):
+        stop = start + len(lst[i::cols])
+        yield lst[start:stop]
+        start = stop
