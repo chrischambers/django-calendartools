0a8ef684930c34ddd03fe48f2d404914ff16510b
Chris Chambers <magma.chambers@gmail.com>
2010-11-10 18:46:28 +0000

    Adding decent Calendar admin defaults

--- calendartools/admin.py
+++ calendartools/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from calendartools.models import Calendar, Event, Occurrence
 
 class CalendarAdmin(admin.ModelAdmin):
-    pass
-
-class EventAdmin(admin.ModelAdmin):
     raw_id_fields = ['creator', 'editor']
     prepopulated_fields = {"slug": ("name",)}
     readonly_fields = ('creator', 'editor', 'datetime_created', 'datetime_modified')
@@ -13,11 +10,15 @@ class EventAdmin(admin.ModelAdmin):
     date_hierarchy = "datetime_created"
     search_fields = ('name',)
 
+class EventAdmin(CalendarAdmin):
+    pass
+
 
 class OccurrenceAdmin(admin.ModelAdmin):
     raw_id_fields = ['creator', 'editor']
     readonly_fields = ('creator', 'editor', 'datetime_created', 'datetime_modified')
-    list_display = ['event', 'start', 'finish', 'status', 'datetime_created']
+    list_display = ['calendar', 'event', 'start', 'finish', 'status',
+                    'datetime_created']
     list_editable = ['status']
     date_hierarchy = "datetime_created"
     search_fields = ('event__name',)
