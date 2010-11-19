1344b7458052fde7e09d2c7e6163aec7858ec0f0
Chris Chambers <magma.chambers@gmail.com>
2010-11-19 22:54:20 +0000

    Better implementation of url override

--- calendartools/templatetags/url_override.py
+++ calendartools/templatetags/url_override.py
@@ -1,12 +1,12 @@
 from django import template
-from django.template import TemplateSyntaxError
+from django.template.defaulttags import URLNode as OrigURLNode
+from django.template.defaulttags import url as orig_url
 from django.utils import translation
 
 register = template.Library()
-from django.template.defaulttags import URLNode as OrigURLNode
 
 class URLNode(OrigURLNode):
-    """Forces all urls to avoid translation"""
+    """Overrides urls to avoid translation."""
 
     def render(self, context):
         language = translation.get_language()
@@ -15,99 +15,8 @@ class URLNode(OrigURLNode):
         translation.activate(language)
         return resolved_url
 
-
-from django.template.defaulttags import kwarg_re
-
 def url(parser, token):
-    """
-    Returns an absolute URL matching given view with its parameters.
+    n = orig_url(parser, token)
+    return URLNode(n.view_name, n.args, n.kwargs, n.asvar)
 
-    This is a way to define links that aren't tied to a particular URL
-    configuration::
-
-        {% url path.to.some_view arg1 arg2 %}
-
-        or
-
-        {% url path.to.some_view name1=value1 name2=value2 %}
-
-    The first argument is a path to a view. It can be an absolute python path
-    or just ``app_name.view_name`` without the project name if the view is
-    located inside the project.  Other arguments are comma-separated values
-    that will be filled in place of positional and keyword arguments in the
-    URL. All arguments for the URL should be present.
-
-    For example if you have a view ``app_name.client`` taking client's id and
-    the corresponding line in a URLconf looks like this::
-
-        ('^client/(\d+)/$', 'app_name.client')
-
-    and this app's URLconf is included into the project's URLconf under some
-    path::
-
-        ('^clients/', include('project_name.app_name.urls'))
-
-    then in a template you can create a link for a certain client like this::
-
-        {% url app_name.client client.id %}
-
-    The URL will look like ``/clients/client/123/``.
-    """
-    bits = token.split_contents()
-    if len(bits) < 2:
-        raise TemplateSyntaxError("'%s' takes at least one argument"
-                                  " (path to a view)" % bits[0])
-    viewname = bits[1]
-    args = []
-    kwargs = {}
-    asvar = None
-    bits = bits[2:]
-    if len(bits) >= 2 and bits[-2] == 'as':
-        asvar = bits[-1]
-        bits = bits[:-2]
-
-    # Backwards compatibility: check for the old comma separated format
-    # {% url urlname arg1,arg2 %}
-    # Initial check - that the first space separated bit has a comma in it
-    if bits and ',' in bits[0]:
-        check_old_format = True
-        # In order to *really* be old format, there must be a comma
-        # in *every* space separated bit, except the last.
-        for bit in bits[1:-1]:
-            if ',' not in bit:
-                # No comma in this bit. Either the comma we found
-                # in bit 1 was a false positive (e.g., comma in a string),
-                # or there is a syntax problem with missing commas
-                check_old_format = False
-                break
-    else:
-        # No comma found - must be new format.
-        check_old_format = False
-
-    if check_old_format:
-        # Confirm that this is old format by trying to parse the first
-        # argument. An exception will be raised if the comma is
-        # unexpected (i.e. outside of a static string).
-        match = kwarg_re.match(bits[0])
-        if match:
-            value = match.groups()[1]
-            try:
-                parser.compile_filter(value)
-            except TemplateSyntaxError:
-                bits = ''.join(bits).split(',')
-
-    # Now all the bits are parsed into new format,
-    # process them as template vars
-    if len(bits):
-        for bit in bits:
-            match = kwarg_re.match(bit)
-            if not match:
-                raise TemplateSyntaxError("Malformed arguments to url tag")
-            name, value = match.groups()
-            if name:
-                kwargs[name] = parser.compile_filter(value)
-            else:
-                args.append(parser.compile_filter(value))
-
-    return URLNode(viewname, args, kwargs, asvar)
 url = register.tag(url)
